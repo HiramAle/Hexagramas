582fef7e2b02b3c12389f54422aa97909aca721b
kat <>
2021-01-06 19:21:02 -0600

    botones e inputs actions

---
+++ public_html/js/hexagramGenerator.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function onFocusInput(e){
+    e.placeholder='';
+}
+
+function onBlurInput(e){
+    e.placeholder='2';
+}
+
+
+
+
