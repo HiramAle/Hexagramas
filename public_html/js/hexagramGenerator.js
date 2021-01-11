89d4f0e84c23e369f594ad8e685284d0d159f69e
kat <DELL@DESKTOP-G7C0GBR>
2021-01-11 00:29:24 -0600

    functions

--- public_html/js/hexagramGenerator.js
+++ public_html/js/hexagramGenerator.js
@@ -3,27 +3,57 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var line_values = [];
+console.log(line_values);
 
-function onFocusInput(e){
-    e.placeholder='';
+function onFocusInput(e) {
+    e.placeholder = '';
 }
 
-function onBlurInput(e){
-    e.placeholder='2';
+function onBlurInput(e) {
+    e.placeholder = '2';
 }
 
-function addLine(){
-    
+function addLine() {
+    var num1 = document.getElementById("digit_1");
+    var num2 = document.getElementById("digit_2");
+    var num3 = document.getElementById("digit_3");
+
+    if ((num1.value.length == 0) || (num2.value.length == 0) || (num3.value.length == 0)) {
+        alert("Debe llenar los tres campos.");
+    } else {
+        if (!(line_values.length == 6)) {
+            var suma = parseInt(num1.value) + parseInt(num2.value) + parseInt(num3.value);
+            line_values.push(suma);
+            resetInputs();
+            console.log(line_values);
+        } else {
+            alert("Un hexagrama solo puede contener 6 lineas.");
+        }
+    }
 }
 
-function eraseLine(){
-    
+function eraseLine() {
+    line_values.pop();
+    console.log(line_values);
 }
 
-function eraseHexagram(){
-    
+function eraseHexagram() {
+    resetInputs();
+    line_values = [];
+    console.log(line_values);
 }
 
+function isValidNumber(e) {
+    var keyCode = e.which ? e.which : e.keyCode;
+    var isValid = ((keyCode >= 50 && keyCode <= 51));
+    if (!isValid)
+        alert("El valor introducido es invalido, solo son aceptados los valores '2' y '3'.");
+    return isValid;
+}
 
-
-
+function resetInputs() {
+    document.getElementById("digit_1").value = "";
+    document.getElementById("digit_2").value = "";
+    document.getElementById("digit_3").value = "";
+}
